a5f7e6ee2d8d5b0a86d0eb0d2412c50d423e0d25
İsmail Hakkı Oruç <138728609+ismailhkk@users.noreply.github.com>
2024-04-18 14:35:04 +0300

    Add files via upload

---
+++ TasKagıtMaka/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace TasKagıtMaka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Taş-Kağıt-Makas oyununa Hoşgeldiniz");
+            string[] secenekler = ["taş", "kağıt", "makas" ];
+            int KazanılanOyun = 0;
+            int KaybedilenOyun = 0;
+            string KullanıcıSecimi = "";
+            Random random = new Random();
+
+
+            while (true)
+            {
+                Console.WriteLine("Yapamak istediğiniz seçimi yazınız.");
+                Console.WriteLine("taş|kağıt|makas|çıkış");
+                KullanıcıSecimi=Console.ReadLine();
+
+                if (KullanıcıSecimi == "çıkış")
+                {
+                    Console.WriteLine("Çıkış Yapılıyor.İyi Günler...");
+                    break;
+
+                }
+
+                int bilgisayarSecimiIndex=random.Next(secenekler.Length);
+                string bilgisayarSecimi = secenekler[bilgisayarSecimiIndex];
+
+                Console.WriteLine(bilgisayarSecimi);
+                if (KullanıcıSecimi == bilgisayarSecimi)
+                {
+                    Console.WriteLine("Berabere..");
+                    continue;
+                }
+
+
+                if (KullanıcıSecimi=="taş" && bilgisayarSecimi=="makas")
+                {
+                    Console.WriteLine("Taş makası kırar Tebrikler Kazandın");
+                    KazanılanOyun++;
+                }
+                else if( KullanıcıSecimi == "kağıt" && bilgisayarSecimi == "taş")
+                {
+                    Console.WriteLine("KAğıt taşı sarar Tebrikler Kazandın");
+                    KazanılanOyun++;
+                }
+                else if(KullanıcıSecimi=="makas" && bilgisayarSecimi == "kağıt")
+                {
+                    Console.WriteLine("Makas Kağıdı keser kazandınız");
+                    KazanılanOyun++;
+                }
+                else
+                {
+                    Console.WriteLine("Kaybettin... Tekrar dene");
+                    KaybedilenOyun++;
+                }
+
+
+
+
+                Console.WriteLine($"kazanılan oyun: { KazanılanOyun}  ,kaybedilen oyun: {KaybedilenOyun}");
+            }
+        }
+    }
+}
